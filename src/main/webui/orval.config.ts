97ac32c156807d8f2f127c211760334d1e415f96
Andras Fejes <fejes.andras@dev-labs.com>
2025-10-08 15:30:19 +0200

    ✨Replaced vite with angular on UI. Generated client with appropriate type-signatures using oval.

---
+++ src/main/webui/orval.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  catalog: {
+    /** The path of the generated OpenAPI scheme */
+    input: 'api/message-template.yaml',
+    output: {
+      /** Generates the scheme and client implementation in different files */
+      mode: 'split',
+      /**  Path to the folder where you want to generate all your models. */
+      schemas: 'src/app/api/model',
+      /** Path to the file which will contains the client implementation. */
+      target: 'src/app/api/service/message-template.ts',
+      /** Type of the client that we use */
+      client: 'angular',
+      /** Base url of the API; it is generated to the client implementation. It should be configuration value. */
+      baseUrl: 'http://localhost:8080'
+    },
+    hooks: {
+      afterAllFilesWrite: 'prettier --write',
+    },
+  },
+});
