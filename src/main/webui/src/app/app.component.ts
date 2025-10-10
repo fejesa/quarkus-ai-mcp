45bfb48901b03c270ff6a5777b8deb603c641caa
Andras Fejes <fejes.andras@dev-labs.com>
2025-10-10 11:24:26 +0200

    ✨Called backend from UI to generate template

--- src/main/webui/src/app/app.component.ts
+++ src/main/webui/src/app/app.component.ts
@@ -1,37 +1,56 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { EditorModule } from 'primeng/editor';
-import { MessageModule } from 'primeng/message';
-import { CommonModule } from '@angular/common';
-import { FloatLabelModule } from 'primeng/floatlabel';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {RouterOutlet} from '@angular/router';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {EditorModule} from 'primeng/editor';
+import {MessageModule} from 'primeng/message';
+import {CommonModule} from '@angular/common';
+import {FloatLabelModule} from 'primeng/floatlabel';
+import {MessageTemplateAPIService} from "./api/service/message-template.service";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    InputTextModule,
-    ButtonModule,
-    EditorModule,
-    MessageModule,
-    FormsModule,
-    FloatLabelModule
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        CommonModule,
+        RouterOutlet,
+        InputTextModule,
+        ButtonModule,
+        EditorModule,
+        MessageModule,
+        FormsModule,
+        FloatLabelModule
+    ],
+    providers: [
+        MessageTemplateAPIService
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  templateDescription = '';
+    templateDescription = '';
 
-  msg = '';
+    msg = '';
 
-  templateContent = '';
+    templateContent = '';
 
-  onClick() {
-    this.msg = 'Sent to LLM';
-  }
+    constructor(private messageTemplateAPIService: MessageTemplateAPIService) {
+    }
+
+    onClick() {
+        this.msg = 'Sent to LLM';
+        this.messageTemplateAPIService.postApi(({
+            templateContent: this.templateContent,
+            description: this.templateDescription
+        })).subscribe({
+            next: (response) => {
+                this.templateContent = response;
+            },
+            error: (error) => {
+                this.msg = 'Error: ' + error.message;
+                console.error(error);
+            }
+        });
+    }
 }
--- src/main/webui/src/app/app.config.ts
+++ src/main/webui/src/app/app.config.ts
@@ -1,21 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { providePrimeNG } from "primeng/config";
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
+import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
+import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import {providePrimeNG} from "primeng/config";
 import Aura from "@primeuix/themes/aura";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura,
-      },
-    })
-  ]
+    providers: [
+        provideBrowserGlobalErrorListeners(),
+        provideZoneChangeDetection({eventCoalescing: true}),
+        provideRouter(routes), provideClientHydration(withEventReplay()),
+        provideHttpClient(withFetch()),
+        provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura,
+            },
+        })
+    ]
 };
