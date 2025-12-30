85066865260f85cbe6c79e76d79f7e781a14ae3a
Andras Fejes <fejes.andras@dev-labs.com>
2025-12-30 22:01:14 +0100

    Generated API and model in the same folder.

--- src/main/webui/orval.config.ts
+++ src/main/webui/orval.config.ts
@@ -4,16 +4,17 @@ export default defineConfig({
   catalog: {
     /** The path of the generated OpenAPI scheme */
     input: 'api/message-template.yaml',
+    mode: 'single',
     output: {
       /** Generates the scheme and client implementation in different files */
       mode: 'split',
       /**  Path to the folder where you want to generate all your models. */
-      schemas: 'src/app/api/model',
-      /** Path to the file which will contains the client implementation. */
+      schemas: 'src/app/api/service',
+      /** Path to the file which will contain the client implementation. */
       target: 'src/app/api/service/message-template.ts',
       /** Type of the client that we use */
       client: 'angular',
-      /** Base url of the API; it is generated to the client implementation. It should be configuration value. */
+      /** Base url of the API; it is generated to the client implementation. It should be a configuration value. */
       baseUrl: 'http://localhost:8080'
     },
     hooks: {
